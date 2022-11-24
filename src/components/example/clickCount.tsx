4c463b3ae33b7e2aa5435592bd436baf2a18fb24
lockey2021 <97558224+lockey2021@users.noreply.github.com>
2022-11-24 15:12:46 +0200

    seventh upload

--- src/components/example/clickCount.tsx
+++ src/components/example/clickCount.tsx
@@ -1,5 +1,5 @@
 import styles from './clickCount.module.scss'
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 type ClickCountProps = {
 
@@ -7,7 +7,10 @@ type ClickCountProps = {
 
 const ClickCount: React.FC<ClickCountProps> = () => {
   const [count, setCount] = useState(0);
-  return (
+  useEffect(() => {
+    console.log(`Clicked the button ${count}`);
+});
+    return (
     <div className={styles.container}>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
