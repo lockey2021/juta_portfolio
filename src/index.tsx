ed17af114ebcc6229e3665f77daec88b43583097
lockey2021 <97558224+lockey2021@users.noreply.github.com>
2022-11-29 14:48:28 +0200

    eigth upload

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import './index.module.scss';
+import SomeExampleComponent from './components/example/someExampleComponent';
+import MyFirstComponent from './components/myFirstComponent';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='/example' element={<SomeExampleComponent />} />
+        <Route path='/first' element={<MyFirstComponent />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
