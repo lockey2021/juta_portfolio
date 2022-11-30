a7c4f33a57212ff24f32573b4a1cd4a712cdc23c
lockey2021 <97558224+lockey2021@users.noreply.github.com>
2022-11-30 12:58:37 +0200

    eleventh upload

---
+++ src/components/aboutMe/aboutMe.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type AboutMeProps = {
+
+}
+
+const AboutMe: React.FC<AboutMeProps> = () => {
+  return (
+    <div>
+      AboutMe
+    </div>
+  )
+}
+
+export default AboutMe
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type FooterProps = {
+
+}
+
+const Footer: React.FC<FooterProps> = () => {
+  return (
+    <div>
+      Footer
+    </div>
+  )
+}
+
+export default Footer
