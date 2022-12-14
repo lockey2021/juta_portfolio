3ea4e19eab781ef30cbded32edd55fe97d8d76be
lockey2021 <97558224+lockey2021@users.noreply.github.com>
2022-12-14 15:35:20 +0200

    hopeful

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import styles from './footer.module.scss'
 import {FaLinkedin} from 'react-icons/fa'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 type FooterProps = {
 
@@ -9,29 +10,38 @@ type FooterProps = {
 const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className={styles.footercontainer}>
-    <div className={styles.footerLeft}>
-      <h3>Juta's portfolio</h3>
-      <p className={styles.footlinks}>
-        <a href="/">Home</a>
-      </p>
+      <div className={styles.footerLeft}>
+        <h3>Juta's portfolio</h3>
+        <p className={styles.footlinks}>
+          <ul>
+            <li>
+            <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/aboutme">About me</Link>
+            </li>
+          </ul>
+                
+                
+          </p>
+      
       <p className={styles.copyright}>Juta Lockey © 2022</p>
       <div className={styles.linksIcon}>
-        <a href='https://www.linkedin.com/in/juta-lockey-03080868'><i><FaLinkedin size={36} color="pink"/></i>
+        <a href='https://www.linkedin.com/in/juta-lockey-03080868'><i><FaLinkedin size={36} color="pink" /></i>
         </a>
       </div>
-    </div>
-    <div className={styles.footerRight}>
-      <p>Contact me if you dare</p>
-      <form action='mailto:jutamaria@hotmail.co.uk' method='post'>
-        <input type='text' name='message' placeholder='Email'>
-        </input>
+    </div><div className={styles.footerRight}>
+        <p>Contact me if you dare</p>
+        <form action='mailto:jutamaria@hotmail.co.uk' method='post'>
+          <input type='text' name='message' placeholder='Email'>
+          </input>
           <textarea name='message' placeholder='Message'>
 
           </textarea>
-          <button type="submit" value= 'Submit'>Send</button>
-        
-      </form>
-    </div>
+          <button type="submit" value='Submit'>Send</button>
+
+        </form>
+      </div>
     </footer>
   )
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -24,8 +24,8 @@ const Navbar: React.FC<NavbarProps> = () => {
       {open && (
       <div className={styles.dropDown}>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/aboutme">About me</a></li>
+          <li><Link to="/" className={styles.dropLink}>Home</Link></li>
+          <li><Link to="/aboutme" className={styles.dropLink}>About me</Link></li>
         </ul>
       </div>
       )}
